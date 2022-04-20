7bc7a76ff13c8bb26997717f18413ef97f591d6f
bba8557 <bba8557@naver.com>
2022-04-20 22:32:40 +0900

    22.04.20 Ex05 수정

--- src/com/javaex/practice/Ex05.java
+++ src/com/javaex/practice/Ex05.java
@@ -6,13 +6,13 @@ public class Ex05 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int sum = 0;
-		
-		for(int i=0; i<5; i++) {
-		int num = sc.nextInt();
-		sum=num+num;
+		int[] num = new int[5];
+		double sum = 0;
+		for(int i=0; i<num.length; i++) {
+		num[i] = sc.nextInt();
+		sum=num[0]+num[1]+num[2]+num[3]+num[4];
 		}
-		System.out.println("평균은 "+sum+" 입니다.");
+		System.out.println("평균은 "+sum/5+" 입니다.");
 		sc.close();
 		
 	}
