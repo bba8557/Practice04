f4cd5a43efeb5a3b0b055f0c6de6a9a3c5df8ed3
bba8557 <bba8557@naver.com>
2022-04-20 23:46:21 +0900

    22.04.20 Practice04

--- src/com/javaex/practice/Ex06.java
+++ src/com/javaex/practice/Ex06.java
@@ -2,10 +2,30 @@ package com.javaex.practice;
 
 public class Ex06 {
 	public static void main(String[] args) {
-		char c[] = {'T','h','i','s',',','i','s',',','a',',','p','e','n','c','i','1'};
+		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','1'};
 		
 		for(int i=0; i<c.length; i++) {
 			System.out.print(c[i]);
+			c[4]=','; c[7]=','; c[9]=',';
 		}
+		
 	}
 }
+/*
+  c[0] = T
+  c[1] = h
+  c[2] = i
+  c[3] = s
+  c[4] = 
+  c[5] = i
+  c[6] = s
+  c[7] = 
+  c[8] = a
+  c[9] = 
+  c[10] = p
+  c[11] = e
+  c[12] = n
+  c[13] = c
+  c[14] = i
+  c[15] = 1
+ */
